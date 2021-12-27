caeba540112283c9d6185d4cf124412da2e7198a
Jim Rourke <jmrourke@yahoo.com>
2021-12-27 08:41:20 -0500

    Implement map services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/rourke/sfgpetclinic/services/map/AbstractMapService.java
@@ -0,0 +1,36 @@
+package com.rourke.sfgpetclinic.services.map;
+
+/* Jim created on 12/24/2021 
+inside the package - com.rourke.sfgpetclinic.services.map */
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, ID> {
+
+    protected Map<ID, T> map = new HashMap<>();
+
+    Set<T> findAll() {
+        return new HashSet<>(map.values());
+    }
+
+    T findById(ID id) {
+        return map.get(id);
+    }
+
+    T save(ID id, T object) {
+        map.put(id, object);
+        return object;
+    }
+
+    void deleteById(ID id) {
+        map.remove(id);
+    }
+
+    void delete(T object) {
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+}
